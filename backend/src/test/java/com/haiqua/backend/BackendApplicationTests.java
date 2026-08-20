7b0e7e686710638dca93ff02d3b3937fab5d2487
haiqua munir <haiquamunir@gmail.com.com>
2026-08-20 18:40:36 +0500

    Add CI workflow for backend test cases

--- backend/src/test/java/com/haiqua/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/haiqua/backend/BackendApplicationTests.java
@@ -1,13 +1,17 @@
 package com.haiqua.backend;
 
+import com.haiqua.backend.config.TestMailConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestMailConfig.class)
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
